e33367b3cae2a525ca57b4200df4a3e059d57134
junho01052 <junho010522@gmail.com>
2023-02-09 08:25:54 +0900

    CSS 추가

--- src/Pages/ToDo.js
+++ src/Pages/ToDo.js
@@ -7,7 +7,7 @@ const Todo = () => {
   const [reFetch, setReFetch] = useState(false);
 
   let Token = "";
-  if (!localStorage.getItem("access_Token")) {    
+  if (!localStorage.getItem("access_Token")) {
     window.location.replace("/signin");
   } else {
     Token = localStorage.getItem("access_Token");
@@ -26,4 +26,10 @@ const Todo = () => {
 
 export default Todo;
 
-const ToDoStyle = styled.div``;
+const ToDoStyle = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin-top: 30vh;
+`;
